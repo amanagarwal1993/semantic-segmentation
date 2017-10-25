b9f0a4f3404d5bb7f1382268228ac53dddfad382
Aman Agarwal <amanagarwal@Amans-MacBook-Air-2.local>
2017-10-25 10:58:18 +0530

    Modified shell script

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-git clone https://github.com/amanagarwal1993/semantic-segmentation.git
 wget http://kitti.is.tue.mpg.de/kitti/data_road.zip
 unzip data_road.zip
 mv data_road semantic-segmentation/data
